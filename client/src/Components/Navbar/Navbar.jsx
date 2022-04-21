40e308033b71e17ff5ac6a7d4da228c434900ab6
Shaad <ssidique610@gmail.com>
2022-04-21 06:18:14 +0530

    fix for main-carousel and special-products

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { IoSettingsOutline } from 'react-icons/io5';
-import { FaBars } from 'react-icons/fa';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { RiArrowDropDownLine } from 'react-icons/ri';
-import { lightTheme } from '../../Theme/LightTheme';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { lightTheme } from "../../Theme/LightTheme";
 
 const Wrapper = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 3rem 7.4rem;
+  padding: 2rem 7.4rem;
   max-height: 5rem;
   width: 100%;
 `;
@@ -65,27 +65,27 @@ const Navbar = () => {
 
       <IconWrapper>SHOPILIVE</IconWrapper>
       <ButtonBox>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>HOME</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>SHOP</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>PRODUCTS</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>FEATURES</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>PAGES</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
-        <Button href='#!'>
+        <Button href="#!">
           <Para>BLOGS</Para>
           <RiArrowDropDownLine size={28} />
         </Button>
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Navbar from '../../Components/Navbar/Navbar';
-import Carousel from '../../Components/Carousel/Carousel';
-import Footer from '../../Components/Footer/Footer';
-import SpecialProducts from '../../Components/SpecialProducts/SpecialProducts';
-import Topbar from '../../Components/Topbar/Topbar';
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Carousel from "../../Components/Carousel/Carousel";
+import Footer from "../../Components/Footer/Footer";
+import SpecialProducts from "../../Components/SpecialProducts/SpecialProducts";
+import Topbar from "../../Components/Topbar/Topbar";
 const Home = () => {
   return (
     <>
