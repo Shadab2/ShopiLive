74144c21472ae8540825eaad6be0d3fd85605262
kanisht09 <kanisht09@gmail.com>
2022-07-15 20:56:11 +0530

    Links updated

---
+++ client/src/Data/simpleLinkData.js
@@ -0,0 +1,23 @@
+const linkNames = [
+  [
+    'Tools',
+    'Christmas',
+    'Marketplace',
+    'Game',
+    'Gym',
+    'Marijuana',
+    'Left-Sidebar',
+    'Jewellery',
+    'Pets',
+    'Metros',
+    'Video-Slider',
+    'Nursery',
+  ],
+  ['Fashion 1', 'Fashion 2', 'Fashion 3', 'Kids'],
+  ['Lookbook', 'Video', 'Parallax', 'Full Page'],
+  ['Electronic-1', 'Electronic-2'],
+  ['Left Sidebar', 'Right Sidebar', 'No Sidebar'],
+  ['Left Image', 'Right Image', 'Image Outside'],
+  ['Thumbnail Left', 'Thumbnail Right', 'Thumbnail Bottom'],
+];
+export { linkNames };
